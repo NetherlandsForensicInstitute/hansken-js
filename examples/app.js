02fc271d314161c8aadedae7c8e85a8dc9c15b71
Gertjan Al (DBS) <g.al@nfi.nl>
2021-11-22 15:26:35 +0100

    Remove old code

--- examples/app.js
+++ examples/app.js
@@ -1,9 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-//app.use(express.static('.'));
-const path = require('path')
+const path = require('path');
 app.use('/', express.static(path.join(__dirname, '/')));
 app.use('/src', express.static(path.join(__dirname, '../src')));
 
